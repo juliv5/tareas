4d9bd0995f6fc55c93582e2c83a52e37bb351998
Juliana Velandia <jvelandia@umariana.edu.co>
2024-04-15 01:54:24 -0500

    Update tareas.component.ts

--- src/app/components/tareas/tareas.component.ts
+++ src/app/components/tareas/tareas.component.ts
@@ -1,22 +1,25 @@
 import { Component } from '@angular/core';
 import { Tarea } from '../../models/Tarea';
 import { FormsModule } from '@angular/forms';
-
-
+import { EMPTY } from 'rxjs';
 
 @Component({
   selector: 'app-tareas',
   standalone: true,
-  imports: [FormsModule,],
+  imports: [FormsModule],
   templateUrl: './tareas.component.html',
-  styleUrl: './tareas.component.scss'
+  styleUrl: './tareas.component.scss',
 })
 export class TareasComponent {
-listTareas: Tarea[] =[];
-nombreTarea = '';
+  listTareas: Tarea[] = [];
+  nombreTarea = '';
 
-
-addTareas(){
-  console.log(this.nombreTarea)
-}
+  addTareas() {
+    const tarea: Tarea = {
+      nombre: this.nombreTarea,
+      estado: false,
+    };
+    this.listTareas.push(tarea);
+    this.nombreTarea='';
+  }
 }
