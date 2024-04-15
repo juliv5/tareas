c6faedff6d7445a370e61868bcb522785cdeff00
Juliana Velandia <jvelandia@umariana.edu.co>
2024-04-15 03:17:27 -0500

    Update tareas.component.ts

--- src/app/components/tareas/tareas.component.ts
+++ src/app/components/tareas/tareas.component.ts
@@ -2,27 +2,37 @@ import { Component } from '@angular/core';
 import { Tarea } from '../../models/Tarea';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { AppComponent } from '../../app.component';
-
-
 
 @Component({
   selector: 'app-tareas',
   standalone: true,
-  imports: [FormsModule,CommonModule,AppComponent],
+  imports: [FormsModule, CommonModule],
   templateUrl: './tareas.component.html',
   styleUrl: './tareas.component.scss',
 })
 export class TareasComponent {
-  listTareas: Tarea [] = [];
+  listTareas: Tarea[] = [];
   nombreTarea = '';
+  mostrarBoton: boolean = false;
 
+  constructor() {
+    this.mostrarBoton = this.detectarDispositivoMovil();
+  }
+  detectarDispositivoMovil(): boolean {
+    return window.innerWidth <= 800; 
+  }
   addTareas() {
-    const tarea: Tarea = {
-      nombre: this.nombreTarea,
-      estado: false,
-    };
-    this.listTareas.push(tarea);
-    this.nombreTarea='';
+    const trimmedNombre = this.nombreTarea.trim();
+
+    if (trimmedNombre !== '') {
+      const tarea: Tarea = {
+        nombre: trimmedNombre,
+        estado: false,
+      };
+      this.listTareas.push(tarea);
+      this.nombreTarea = '';
+    } else {
+      alert('Por favor, ingrese un nombre válido para la tarea.');
+    }
   }
 }
